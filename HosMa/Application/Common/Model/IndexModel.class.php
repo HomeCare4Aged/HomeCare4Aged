1a0011e72f19bf39fdb05656aef568666c436a35
fgxiao <1324850311@qq.com>
2016-12-16 17:31:13 +0800

    2016-12-16-w

--- HosMa/Application/Common/Model/IndexModel.class.php
+++ HosMa/Application/Common/Model/IndexModel.class.php
@@ -3,12 +3,32 @@ namespace Common\Model;
 use Think\Model;
 
 class IndexModel extends Model{
-	public function index(){
-		
-		$this->display();
+	//获取菜单信息
+	public function getDoctors($cond=array(),$page=1,$pageSize=10){
+			$this->where($cond); //查询条件
+//				->order('list_order desc,article_id desc');     //排序规则
+		//处理分页的业务
+		if($page === null || $pageSize !== null){
+			$page = $page !== null ? $page : 1;
+			$pageSize = $pageSize !== null ? $pageSize : 10;
+			//每页的起点（偏移量）
+			$offset = ($page - 1) * $pageSize;
+			$this->limit($offset,$pageSize);    //查询条数
+		}
+			$list = $this->select();
+		return $list;
 	}
-	public function add(){
-		$this->display();
-	};
+	//获取菜单组数
+	public function getMenusCount($cond=array()){
+		return $this->where($cond)->count();
+	}
+	//按照id查询
+	public function findMenuById($id){
+		if($id == null){
+			throw_exception('医生ID不合法');
+		}
+		$cond['article_id'] = intval($id);
+		return $this->where($cond)->find();
+	}
 }
 ?>
