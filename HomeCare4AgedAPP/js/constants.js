470e49ab32deabc858f889df7d27a158a4c0427f
gaodehao <695003889@qq.com>
2016-12-19 10:16:57 +0800

    gao

--- HomeCare4AgedAPP/js/constants.js
+++ HomeCare4AgedAPP/js/constants.js
@@ -1,7 +1,9 @@
-<<<<<<< HEAD
+
 //域名
 const SERVERURL = 'http://10.96.112.49/HomeCare4AgedPHP/Home/';
-=======
 //定义比例
 const SCALE = window.screen.width/320;
->>>>>>> b5572b82e39faee30fa5b4b2c5b3d3df3ae4e2de
+//定义首页图片比例
+const HOMEIMGSCALE = 620/498;
+//定义首页顶部图片比例
+const HOMETOPIMGSCALE = 620/1005;
