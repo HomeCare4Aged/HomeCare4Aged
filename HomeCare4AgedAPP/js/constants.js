b5cd96fabca6d833ecdd609d60f39c59b3492f38
“zhao0810 <your_registere945092644@qq.com>
2016-12-22 11:25:43 +0800

    zhao

--- HomeCare4AgedAPP/js/constants.js
+++ HomeCare4AgedAPP/js/constants.js
@@ -1,7 +1,7 @@
 
 //域名
 
-const SERVERURL = 'http://10.96.105.221/HomeCare4AgedPHP/index.php/Home/';
+const SERVERURL = 'http://192.168.136.1/HomeCare4AgedPHP/index.php/Home/';
 //const SERVERURL = 'http://192.168.136.1/HomeCare4AgedPHP/index.php/Home/';
 //定义比例
 const SCALE = window.screen.width/320;
