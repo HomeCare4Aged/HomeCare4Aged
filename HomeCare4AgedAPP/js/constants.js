3622c218adff565030e7fbab6374fc5c6b3df95d
gaodehao <695003889@qq.com>
2016-12-27 09:59:51 +0800

    gao

--- HomeCare4AgedAPP/js/constants.js
+++ HomeCare4AgedAPP/js/constants.js
@@ -1,15 +1,7 @@
 
 //域名
 
-<<<<<<< HEAD
-
-
-const SERVERURL = 'http://192.168.136.1/HomeCare4AgedPHP/index.php/Home/';
-
-
-=======
-const SERVERURL = 'http://10.96.108.150/HomeCare4AgedPHP/index.php/Home/';
->>>>>>> 67a8c20161069e1f78ce66ff8680198aeee725c1
+const SERVERURL = 'http://10.96.110.77/HomeCare4AgedPHP/index.php/Home/';
 //const SERVERURL = 'http://192.168.136.1/HomeCare4AgedPHP/index.php/Home/';
 
 //定义比例
