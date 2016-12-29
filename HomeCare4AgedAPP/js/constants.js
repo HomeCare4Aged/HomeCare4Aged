4073aa114030802e489e48012df8eacb0e253e92
“zhao0810 <your_registere945092644@qq.com>
2016-12-29 16:06:28 +0800

    zhao

--- HomeCare4AgedAPP/js/constants.js
+++ HomeCare4AgedAPP/js/constants.js
@@ -3,7 +3,7 @@
 //const SERVERURL = 'http:/10.96.110.77/HomeCare4AgedPHP/index.php/Home/';
 
 const SERVERURL = 'http://192.168.136.1/HomeCare4AgedPHP/index.php/Home/';
-
+const MESSAGEURL = 'http://192.168.136.1/HomeCare4AgedPHP/SendTemplateSMS.php/sendTemplateSMS';
 //定义比例
 const SCALE = window.screen.width / 320;
 //定义首页图片比例
