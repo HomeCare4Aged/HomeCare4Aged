6d2f18846bd14ea7303ce192f647b8f4b326844f
huoye <82694657@qq.com>
2017-01-04 15:44:51 +0800

    hahahah

--- HosMa/Application/Admin/Controller/ScheduleController.class.php
+++ HosMa/Application/Admin/Controller/ScheduleController.class.php
@@ -5,6 +5,7 @@ import("XXKit.Constants");
 
 class ScheduleController extends Controller {
     public function index(){
+    	
     	$date=date('Y-m-d',time()-7*3600*24);
     	$cond['schedule_date'] = array('egt',$date);
 		$schedule = M('h_user_scheduling_list')
@@ -14,21 +15,21 @@ class ScheduleController extends Controller {
 		$doctor=D('h_doctor_base_info')->select();
 //    				cDebug($doctor);
 		//分页
-		$page=$_REQUEST['p'] ?$_REQUEST['p']:1;
-    	$pageSize=$_REQUEST['pageSize']?$_REQUEST['pageSize']:5;
-    	$schedule=D('h_user_scheduling_list')->getSchedules($cond,$page,$pageSize);
-    	$scheduletateCount=D('h_user_scheduling_list')->getMenusCount($cond);
-    	//分页控件
-    	$pageObj = new \Think\Page($scheduletateCount,$pageSize);
-    	//获取分页结果
-    	$pageRes = $pageObj->show();
+//		$page=$_REQUEST['p'] ?$_REQUEST['p']:1;
+//  	$pageSize=$_REQUEST['pageSize']?$_REQUEST['pageSize']:5;
+//  	$schedule=D('h_user_scheduling_list')->getSchedules($cond,$page,$pageSize);
+//  	$scheduletateCount=D('h_user_scheduling_list')->getMenusCount($cond);
+//  	//分页控件
+//  	$pageObj = new \Think\Page($scheduletateCount,$pageSize);
+//  	//获取分页结果
+//  	$pageRes = $pageObj->show();
     	
     	foreach($schedule as $key=>$value){
 			$schedule[$key]['hospital_doctor_name'] = M('h_doctor_base_info')->where(array('hospital_doctor_id' => $value['hospital_doctor_id']))->getField('hospital_doctor_name');
       		
 		}
 
-    	$this->assign('pageRes',$pageRes);
+//  	$this->assign('pageRes',$pageRes);
 		$this->assign('doctor',$doctor);
 		$this->assign('schedule',$schedule);
     	$this->display();
@@ -98,49 +99,36 @@ class ScheduleController extends Controller {
 //  根据名字查找排班信息
     public function getName(){
     	$selectValue = I('selectValue');
-		$cond['hospital_doctor_id']=$selectValue;
-		$schedule=M('h_user_scheduling_list')->where($cond)->select();
-		//分页
-		$page=$_REQUEST['p'] ?$_REQUEST['p']:1;
-    	$pageSize=$_REQUEST['pageSize']?$_REQUEST['pageSize']:5;
-    	$schedule=D('h_user_scheduling_list')->getSchedules($cond,$page,$pageSize);
-    	$scheduletateCount=D('h_user_scheduling_list')->getMenusCount($cond);
-    	//分页控件
-    	$pageObj = new \Think\Page($scheduletateCount,$pageSize);
-    	//获取分页结果
-    	$pageRes = $pageObj->show();
-    	
-		foreach($schedule as $key=>$value){
-			$schedule[$key]['hospital_doctor_name'] = M('h_doctor_base_info')->where(array('hospital_doctor_id' => $value['hospital_doctor_id']))->getField('hospital_doctor_name');
+//  	cDebug($selectValue);
+    	if($selectValue&&$selectValue!=10086){
+    		$date=date('Y-m-d',time()-7*3600*24);
+    		$cond['schedule_date'] = array('egt',$date);
+			$cond['hospital_doctor_id']=$selectValue;
+			$schedule=M('h_user_scheduling_list')->where($cond)->select();
+			
+		}else{
+			$date=date('Y-m-d',time()-7*3600*24);
+    		$cond['schedule_date'] = array('egt',$date);
+			$schedule = M('h_user_scheduling_list')
+					->where($cond)
+					->join('h_doctor_base_info on h_user_scheduling_list.hospital_doctor_id = h_doctor_base_info.hospital_doctor_id')
+					->select();
 		}
-
+    	foreach($schedule as $key=>$value){
+			$schedule[$key]['hospital_doctor_name'] = M('h_doctor_base_info')->where(array('hospital_doctor_id' => $value['hospital_doctor_id']))->getField('hospital_doctor_name');
+			}
 		if($schedule === false){
     		return ajaxReturn(\DATABASE_ERROR,'数据库查询失败！');
     	}
-//		  	cDebug($pageSize);
-    	$res['schedule']=$schedule;
-    	$res['pageRes']=$pageRes;
-//  	cDebug($res);
-    	echo json_encode($res);
-    	
+//  	$res=array('schedule' => $schedule,'pageRes'=>$pageRes);
+    	echo json_encode($schedule);
 
     }
 //  根据时间查找排班信息
     public function getTime(){
     	$timeValue = I('timeValue');
 //  	cDebug($timeValue);
-		
 		$cond['schedule_date']=$timeValue;
-		//分页
-		$page=$_REQUEST['p'] ?$_REQUEST['p']:1;
-    	$pageSize=$_REQUEST['pageSize']?$_REQUEST['pageSize']:5;
-    	$schedule=D('h_user_scheduling_list')->getSchedules($cond,$page,$pageSize);
-    	$scheduletateCount=D('h_user_scheduling_list')->getMenusCount($cond);
-    	//分页控件
-    	$pageObj = new \Think\Page($scheduletateCount,$pageSize);
-    	//获取分页结果
-    	$pageRes = $pageObj->show();
-		
 		$schedule=M('h_user_scheduling_list')->where($cond)->select();
 		foreach($schedule as $key=>$value){
 			$schedule[$key]['hospital_doctor_name'] = M('h_doctor_base_info')->where(array('hospital_doctor_id' => $value['hospital_doctor_id']))->getField('hospital_doctor_name');
