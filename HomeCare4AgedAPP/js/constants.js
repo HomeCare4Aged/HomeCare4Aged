0ef32234e0d4a36ba93f1a2b0dad602c584efe50
“zhao0810 <your_registere945092644@qq.com>
2017-01-13 08:58:11 +0800

    zhao

--- HomeCare4AgedAPP/js/constants.js
+++ HomeCare4AgedAPP/js/constants.js
@@ -32,18 +32,33 @@ function getNowFormatDate() {
     var date = new Date();
     var seperator1 = "-";
     var seperator2 = ":";
+    var year = date.getFullYear();
     var month = date.getMonth() + 1;
     var strDate = date.getDate();
+    var strSeconds = date.getSeconds();
     if (month >= 1 && month <= 9) {
         month = "0" + month;
     }
     if (strDate >= 0 && strDate <= 9) {
         strDate = "0" + strDate;
     }
-    var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
-            + " " + date.getHours() + seperator2 + date.getMinutes()
-            + seperator2 + date.getSeconds();
-    return currentdate;
+    var hours = date.getHours();
+	var minutes = date.getMinutes();
+	var seconds = date.getSeconds();
+	if(hours >= 0 && hours <= 9) {
+		hours = "0" + hours;
+	}
+	if(minutes >= 0 && minutes <= 9) {
+		minutes = "0" + minutes;
+	}
+	if(seconds >= 0 && seconds <= 9) {
+		seconds = "0" + seconds;
+	}
+
+	var currentdate = year + seperator1 + month + seperator1 + strDate +
+		" " + hours + seperator2 + minutes +
+		seperator2 + seconds;
+	return currentdate;
 }
 
 function gmtToDate(str) {
