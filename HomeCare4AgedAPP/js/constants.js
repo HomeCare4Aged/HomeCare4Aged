e910c3747fb56bde509c34ce95b0e2467a81eb9c
“zhao0810 <your_registere945092644@qq.com>
2017-01-21 14:31:47 +0800

    asd

--- HomeCare4AgedAPP/js/constants.js
+++ HomeCare4AgedAPP/js/constants.js
@@ -1,8 +1,8 @@
 //域名
 
 
-const SERVERURL = 'http://139.224.44.36/HomeCare4AgedPHP/index.php/Home/';
-const MESSAGEURL = 'http://139.224.44.36/HomeCare4AgedPHP/SendTemplateSMS.php/sendTemplateSMS';
+const SERVERURL = 'http://192.168.72.1/HomeCare4AgedPHP/index.php/Home/';
+const MESSAGEURL = 'http://192.168.72.1/HomeCare4AgedPHP/SendTemplateSMS.php/sendTemplateSMS';
 
 //定义比例
 const SCALE = window.screen.width / 320;
